af3ccb82a506ad9244c24cff5cfc4351c80565c4
jude <jude.niroshan11@gmail.com>
2022-09-12 14:17:23 +0200

    Boutique shop scripts added

--- src/main/java/com/saas/tenant/manager/service/SubscriptionService.java
+++ src/main/java/com/saas/tenant/manager/service/SubscriptionService.java
@@ -3,22 +3,13 @@ package com.saas.tenant.manager.service;
 import com.saas.tenant.manager.model.Subscription;
 import com.saas.tenant.manager.model.Tenant;
 import com.saas.tenant.manager.repository.SubscriptionRepository;
-import io.fabric8.kubernetes.api.model.GenericKubernetesResource;
-import io.fabric8.kubernetes.api.model.Namespace;
-import io.fabric8.kubernetes.api.model.NamespaceBuilder;
-import io.fabric8.kubernetes.api.model.ObjectMetaBuilder;
-import io.fabric8.kubernetes.client.KubernetesClient;
-import io.fabric8.kubernetes.client.KubernetesClientBuilder;
-import io.fabric8.kubernetes.client.dsl.NamespaceableResource;
-import io.fabric8.openshift.client.OpenShiftClient;
 import org.springframework.stereotype.Service;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
 
 @Service
 public class SubscriptionService {
@@ -48,70 +39,31 @@ public class SubscriptionService {
 
         Subscription savedSubscription = subscriptionRepository.save(subscription);
 
-        String namespaceName = savedSubscription.getTenantName() + System.currentTimeMillis();
-        String freshRssName = savedSubscription.getTenantName();
-        String crdApiVersion = "freshrss.demo.openshift.com/v1alpha1";
-        String crdKind = "FreshRSS";
-        String defaultUser = tenant.getTenantUserName();
+        String namespaceName = savedSubscription.getTenantName().replaceAll("\\s", "-") +
+                System.currentTimeMillis();
 
-//        try (final KubernetesClient k8sClient = new KubernetesClientBuilder().build()) {
-//            // Do stuff with client
-//            OpenShiftClient ocpClient = k8sClient.adapt(OpenShiftClient.class);
-//            Namespace namespace = new NamespaceBuilder()
-//                    .withNewMetadata()
-//                    .withName(namespaceName)
-//                    .endMetadata()
-//                    .build();
-//            ocpClient.namespaces().createOrReplace(namespace);
-//
-//            Map<String, Object> map = new HashMap<>();
-//            map.put("defaultUser", defaultUser);
-//            map.put("title", savedSubscription.getTenantName());
-//
-//            GenericKubernetesResource crdResource = new GenericKubernetesResource();
-//            crdResource.setKind(crdKind);
-//            crdResource.setApiVersion(crdApiVersion);
-//            crdResource.setMetadata(new ObjectMetaBuilder()
-//                    .withNamespace(namespace.getMetadata().getName())
-//                    .withName(freshRssName)
-//                    .build());
-//            crdResource.setAdditionalProperty("spec", map);
-//            NamespaceableResource<GenericKubernetesResource> resource = ocpClient.resource(crdResource);
-//            resource.createOrReplace();
-//
-//            List<String> urlList = new ArrayList<>();
-//
-//            while (urlList.isEmpty()) {
-//                Thread.sleep(200);
-//
-//                List<GenericKubernetesResource> list = ocpClient
-//                        .genericKubernetesResources("freshrss.demo.openshift.com/v1alpha1", crdKind)
-//                        .inNamespace(namespaceName)
-//                        .list().getItems();
-//                list.get(0).getAdditionalProperties().entrySet().stream()
-//                        .filter(e -> e.getKey().equals("status"))
-//                        .findFirst()
-//                        .ifPresent(e -> {
-//                            if (e.getValue() instanceof LinkedHashMap) {
-//                                LinkedHashMap linkedHashMap = (LinkedHashMap) e.getValue();
-//                                String url = (String) linkedHashMap.get("url");
-//                                urlList.add(url);
-//                            }
-//                        });
-//            }
-//
-//            if (!urlList.isEmpty()) {
-//                System.out.println("URL --->" + urlList.get(0));
-//                savedSubscription.setUrl(urlList.get(0));
-//                savedSubscription.setStatus("Active");
-//            } else {
-//                System.out.println("URL couldn't fetch from the FreshRSS crd!!!");
-//            }
-//
-//            ocpClient.close();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
+        try {
+            ProcessBuilder pb = new ProcessBuilder("./src/main/resources/ocp/create-namespace.sh", namespaceName);
+            Process p = pb.start();
+            InputStream is = p.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+
+            String line = null;
+            String lastLine = "";
+            while ((line = reader.readLine()) != null) {
+                lastLine = line;
+            }
+            if (lastLine.contains("http:")) {
+                String routeUrl = lastLine.substring(lastLine.indexOf("http:"));
+                System.out.println("URL --->" + routeUrl);
+                savedSubscription.setUrl(routeUrl);
+                savedSubscription.setStatus("Active");
+            } else {
+                System.out.println("URL couldn't fetch from the FreshRSS crd!!!");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
         Subscription save = subscriptionRepository.save(savedSubscription);
         return save;
